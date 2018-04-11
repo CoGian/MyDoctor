9f0006b23be6d912e2d76792e2df9f37d3cfccb6
georgekal2798 <georgekal2798@users.noreply.github.com>
2018-04-11 14:05:58 +0300

    Changes to the gui elements (temporary)
    Καποια φαινονται χαλια αλλα για να συνεχισω θα το αφησω ετσι προσωρινα και θα ασχοληθω με την εμφανιση αργοτερα

--- src/Auth_GUI.java
+++ src/Auth_GUI.java
@@ -1,10 +1,9 @@
-import java.awt.GridLayout;
+import java.awt.*;
 
 import javax.swing.*;
 
 public class Auth_GUI extends JFrame{
 
-	private JPanel panel = new JPanel(); 
 	private JPanel LoginPanel = new JPanel() ; 
 	private JPanel RegisterPanel = new JPanel() ; 
 	
@@ -12,42 +11,43 @@ public class Auth_GUI extends JFrame{
 	private JButton RegisterButton = new JButton("Register");
 	
 	
-	private JTextField UsernameLogField = new JTextField();
-	private JPasswordField PasswordLogField = new JPasswordField();
+	private JTextField UsernameLogField = new JTextField(20);
+	private JPasswordField PasswordLogField = new JPasswordField(20);
 	
-	private JTextField UsernameRegField = new JTextField();
-	private JPasswordField PasswordRegField = new JPasswordField();
-	private JTextField NameField = new JTextField();
-	private JTextField TelephoneField = new JTextField();
-	private JTextField AddressField = new JTextField();
-	private JTextField EmailField = new JTextField();
-	private JTextField AgeField = new JTextField();
+	private JTextField UsernameRegField = new JTextField(20);
+	private JPasswordField PasswordRegField = new JPasswordField(20);
+	private JTextField NameField = new JTextField(20);
+	private JTextField TelephoneField = new JTextField(20);
+	private JTextField AddressField = new JTextField(20);
+	private JTextField EmailField = new JTextField(20);
+	private JTextField AgeField = new JTextField(20);
 	
 	String [] genders = { "Male", "Female" };
 	private JComboBox GenderField = new JComboBox(genders);
 	
 	public Auth_GUI() {
-		LoginPanel.setLayout(new GridLayout(11,1));
-		
+		LoginPanel.setLayout(new BoxLayout(LoginPanel, BoxLayout.PAGE_AXIS));
+				
 		LoginPanel.add(new JLabel("Username:"));
+		LoginPanel.add(Box.createHorizontalGlue());
 		LoginPanel.add(UsernameLogField);
-		LoginPanel.add(new JLabel("Password: "));
+		LoginPanel.add(new JLabel("Password:"));
 		LoginPanel.add(PasswordLogField);
 		LoginPanel.add(LoginButton);
 		
-		RegisterPanel.setLayout(new GridLayout(17,1));
+		RegisterPanel.setLayout(new BoxLayout(RegisterPanel, BoxLayout.PAGE_AXIS));
 		
 		RegisterPanel.add(new JLabel("Username: "));
 		RegisterPanel.add(UsernameRegField);
 		RegisterPanel.add(new JLabel("Password: "));
 		RegisterPanel.add(PasswordRegField);
-		RegisterPanel.add(new JLabel("Name: "));
+		RegisterPanel.add(new JLabel("Full Name: "));
 		RegisterPanel.add(NameField);
 		RegisterPanel.add(new JLabel("Address: "));
 		RegisterPanel.add(AddressField);
 		RegisterPanel.add(new JLabel("Telephone: "));
 		RegisterPanel.add(TelephoneField);
-		RegisterPanel.add(new JLabel("e-mail: "));
+		RegisterPanel.add(new JLabel("E-mail: "));
 		RegisterPanel.add(EmailField);
 		RegisterPanel.add(new JLabel("Age: "));
 		RegisterPanel.add(AgeField);
@@ -55,12 +55,13 @@ public class Auth_GUI extends JFrame{
 		RegisterPanel.add(GenderField);
 		RegisterPanel.add(RegisterButton);
 				
-		this.setContentPane(panel);
-		this.getContentPane().setLayout(new GridLayout(1,2,30,0));
+		this.getContentPane().setLayout(new BoxLayout(this.getContentPane(), BoxLayout.X_AXIS));
 		this.getContentPane().add(LoginPanel);
+		this.getContentPane().add(Box.createRigidArea(new Dimension(20,0)));
 		this.getContentPane().add(RegisterPanel);
-		this.setSize(800,500);
+		this.setSize(470,400);
 		this.setVisible(true);
+		this.pack();
 		this.setTitle("LOGIN GUI");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
